eaea4a1753f808b9325efba4103046faf0e56ea6
Jose Vte Vento <jvvento@gmail.com>
2015-06-08 12:50:09 +0200

    Primer commit

---
+++ FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Pruebas_ASP_C_sharp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
