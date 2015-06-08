b6708dd35b798828fb9bbfa793c7f7d7f100f181
Jose Vte Vento <jvvento@gmail.com>
2015-06-08 12:59:29 +0200

    Pruebas con github y plataforma ASP .net
    Primer commit de la carpeta controllers

---
+++ HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pruebas_ASP_C_sharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        private String nombre { get; set; }
+
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+
+    }
+}
